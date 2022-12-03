21fd9f15166986c9c2e2b2098ec932b81d904a93
chusamasadiq <usamasadiq163@gmail.com>
2022-12-03 19:03:09 +0500

    eRozgaar

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:erozgaar/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const Erozgaar());
-
 
 class Erozgaar extends StatelessWidget {
   const Erozgaar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      theme: ThemeData(
+        //fontFamily: GoogleFonts.openSans().fontFamily,
+      ),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({Key? key}) : super(key: key);
@@ -8,11 +11,50 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  final List<Marker> _marker = [];
+  final List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(30.7222, 72.6447),
+        infoWindow: InfoWindow(
+          title: 'Kamalia, Meri Jaan!',
+        ))
+  ];
+  static final CameraPosition _kGooglePlex =
+      CameraPosition(target: LatLng(30.7222, 72.6447), zoom: 14.4746);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Location Screen'),
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: Set<Marker>.of(_marker),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0Xff3FBC46),
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(
+            CameraUpdate.newCameraPosition(
+              const CameraPosition(target: LatLng(30.7222, 72.6447), zoom: 14),
+            ),
+          );
+        },
+        child: const Icon(Icons.location_searching, color: Colors.white),
       ),
     );
   }
